cf39d0c0d0212d19f0a0f52d8648dfba97c397a3
Sebastian Walter <sebastian.walter98@gmail.com>
2020-11-27 12:49:41 +0100

    little refactor and added multiprocessing support

--- helper/files.py
+++ helper/files.py
@@ -1,7 +1,12 @@
-from typing import Iterator
+from typing import Iterator, List
 
 
 def read_lines(path: str) -> Iterator[str]:
     with open(path) as f:
         for line in f:
             yield line[:-1]
+
+
+def read_file(path: str) -> List[str]:
+    with open(path, "r", encoding="utf8") as f:
+        return f.readlines()
